c3568893241b08a3ec334e85520a0bd6b0b248e8
quadroloop <nplixel@gmail.com>
2019-03-13 19:02:50 +0800

    finished UI, binding with back end is needed

--- src/js/morphDash.js
+++ src/js/morphDash.js
@@ -11,13 +11,34 @@ function sclass(classx){
       
 
       // Data
+
 var line1 = new TimeSeries();
 var line2 = new TimeSeries();
+var line3 = new TimeSeries();
+
+var humidity = 0;
+var temperature = 0;
+var sound = 0;
+
+var cpage = "dashboard";
+
+function nav(page,btn){
+   sclass('active')[0].classList.remove("active");
+   btn.classList.add('active');
+   el(cpage).style.display = "none";
+   el(page).style.display = "block";
+   el('cpage').innerHTML = page;
+   el('cpage').style = "text-transform: Capitalize;"
+   cpage = page;
+}
+
+
 
 // Add a random value to each line every second
 setInterval(function() {
-  line1.append(new Date().getTime(), Math.random());
-  line2.append(new Date().getTime(), Math.random());
+  line1.append(new Date().getTime(), parseInt(el('hum').innerHTML));
+  line2.append(new Date().getTime(), temperature);
+  line3.append(new Date().getTime(), sound);
 }, 500);
 
 // Add to SmoothieChart
@@ -33,7 +54,7 @@ setInterval(function() {
 
       function createSoundChart(){
       	var schart = new SmoothieChart({responsive:true});
-      	schart.addTimeSeries(line1,{ strokeStyle:'#3F88BF', fillStyle:'rgba(77, 157, 218, 0.68)', lineWidth:2 });
+      	schart.addTimeSeries(line3,{ strokeStyle:'#3F88BF', fillStyle:'rgba(77, 157, 218, 0.68)', lineWidth:2 });
 
         schart.streamTo(document.getElementById("soundchart"), 500);
       }
