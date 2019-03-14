c7f7c7a88bb923bb6d1c8583d0e257b861a2f56d
quadroloop <nplixel@gmail.com>
2019-03-14 15:26:48 +0800

    Goodye rogdrigo :(

--- app.js
+++ app.js
@@ -39,20 +39,196 @@ function guid() {
     });
 }
 
+// get date
+function nDate(){
+  let date = new Date()
+let options = {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit'
+        };
+let formattedDate = date.toLocaleString(undefined, options);
+  return formattedDate;
+}
+
+// get time
+function nTime(){
+  var ts = new Date();
+  return ts.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+}
+
 
 // new api functions here
 
 
-var route = "/sendData";
+
+
+
+// sending data for slower sensors
+var route = "/slowData";
 app.get(route,(req,res)=>{
-  var sensorType = req.query.sensor;
-  var sensorData = req.query.sensorData;
-  var msg = `${sensorType}:${sensorData}`;
-   res.send("ok");
-   console.log(msg);
+
+   var dhthum = req.query.hum;
+   var dhttemp = req.query.temp;
+   var ultrasonic = req.query.ultrasonic;
+   var raindrop = req.query.raindrop;
+   var gas = req.query.gas;
+   var ts = req.query.touch;
+
+   //saving for dht
+   var dht22 = db.get('dht22').value();
+     var dhtData = { 
+       "id": guid(),
+       "humidity": dhthum,
+       "temperature":dhttemp,
+       "date":nDate(),
+       "time":nTime()
+     }
+     dht22.push(dhtData);
+
+   // saving ultrasonic data 
+   var ultrasonicx = db.get('ultrasonic').value();
+     var usdata = {
+       "id": guid(),
+       "reading": ultrasonic,
+       "date": nDate(),
+       "time": nTime()
+      }
+     ultrasonicx.push(usdata); 
+   
+   // saving gas sensor data
+   var raindropSave = db.get('raindrop').value();
+      var rainData = {
+        "id": guid(),
+        "reading": raindrop,
+        "date": nDate(),
+        "time": nTime()
+      }
+      raindropSave.push(rainData);
+
+    //gas sensor
+    var gasSensorx = db.get('gas').value();
+       var gasData = {
+         "id": guid(),       
+         "reading": gas,
+         "date": nDate(),
+         "time": nTime()
+       }
+       gasSensorx.push(gasData);
+
+    db.write();  
+
+    var slowSensors = `
+        temperature: ${dhttemp} 
+        humidity: ${dhthum}
+        ultrasonic: ${ultrasonic}
+        raindrop: ${raindrop}
+        gas: ${gas}
+
+    `;
+
+    var sensorSet1 = {
+        temperature: dhttemp, 
+        humidity: dhthum,
+        ultrasonic: ultrasonic,
+        raindrop: raindrop,
+        gas: gas,
+        touch: ts       
+    }
+    
+    io.emit("s1",sensorSet1);
+
+   res.send("sensor set 1: Ok");
+})
+
+// fast sensors
+var route = "/fastSensors";
+app.get(route,(req,res)=>{
+
+    var sflame = req.query.flame;
+    var slight = req.query.light;
+    var ssound = req.query.sound;
+    var shall = req.query.hall;
+
+    var sensorSet2 = {
+      flame: sflame,
+      light: slight,
+      sound: ssound,
+      hall: shall
+    }
+
+    io.emit("s2",sensorSet2);
+    res.send("sensor set 2: ok");
+
+    // var sensors = req.query.sensor;
+    // var sensorReading = req.query.reading;
+
+    //    switch(sensors){
+    //       // for light sensor
+    //       case 'light':
+    //          var lightTable = db.get('light').value();
+    //             var newData = {
+    //                id: guid(),
+    //                reading: sensorReading,
+    //                date: nDate(),
+    //                time: nTime()
+    //             }
+    //        lightTable.push(newData);
+    //       break;
+
+    //       // for flame sensor
+    //       case 'flame':
+    //        var flameTable = db.get('flame').value();
+    //             var newData = {
+    //                id: guid(),
+    //                reading: sensorReading,
+    //                date: nDate(),
+    //                time: nTime()
+    //             }
+    //        flameTable.push(newData);
+    //       break;
+
+    //       // for sound sensor
+    //       case 'sound':
+    //        var soundTable = db.get('sound').value();
+    //             var newData = {
+    //                id: guid(),
+    //                reading: soundTable,
+    //                date: nDate(),
+    //                time: nTime()
+    //             }
+    //        soundTable.push(newData);
+    //       break;
+
+    //       // for hall effect
+    //       case 'halleffect':
+    //        var hallTable = db.get('halleffect').value();
+    //             var newData = {
+    //                id: guid(),
+    //                reading: sensorReading,
+    //                date: nDate(),
+    //                time: nTime()
+    //             }
+    //        hallTable.push(newData);
+    //       break;
+
+    //    } 
+
+       
+
+       // io.emit(s2,);
+
+
 })
 
 
+// getting data logs
+var route = "/datalogs";
+app.get(route,(req,res)=>{
+    var all = db.value();
+    res.send(all);
+
+})
 
 var port = 3000;
 
--- src/js/morphDash.js
+++ src/js/morphDash.js
@@ -8,9 +8,68 @@ function sclass(classx){
 	return document.getElementsByClassName(classx);
 }
 
+
+// declare websocket server 
+
+var socket = io();
+
+
+// for parsing sensor set = 1
+socket.on("s1",(sensors1)=>{
+    el('hum').innerHTML = parseFloat(sensors1.humidity).toFixed(2);
+    el('temp').innerHTML = parseFloat(sensors1.temperature).toFixed(2);
+    el('us').innerHTML = parseFloat(sensors1.ultrasonic).toFixed(2);
+    el('rd').innerHTML = parseInt(sensors1.raindrop);
+    el('ct').innerHTML = sensors1.touch; 
+
+    var gasReading = parseInt(sensors1.gas);
+        if(gasReading == 1){
+           el('gas').innerHTML = "Normal";
+        }else{
+           el('gas').innerHTML = "Warning!";
+        }
+
+})
+
+
+// for parsing sensor set 2
+
+socket.on("s2",(sensor2)=>{
+   var soundReading = sensor2.sound;
+
+
+   // reading sound
+   if(soundReading == "1"){
+      soundReading = 1+Math.random();
+   }else{
+      soundReading = 0
+   }
+
+
+   // detecting flames
+   if(sensor2.flame == "0"){
+      el('fs').innerHTML = "Warning"; 
+   }else{
+      el('fs').innerHTML = "Normal";
+   }
+
+   //  detecting light
+   el('ls').innerHTML = sensor2.light;
+   el('ssx').innerHTML = soundReading;
+
+  if(sensor2.hall == "0"){
+    el('he').innerHTML = "Magnetized";
+
+  }else{
+    el('he').innerHTML = "Normal"
+  }
+
+   console.log(sensor2);
+})
+
       
 
-      // Data
+// data
 
 var line1 = new TimeSeries();
 var line2 = new TimeSeries();
@@ -22,7 +81,145 @@ var sound = 0;
 
 var cpage = "dashboard";
 
+function parseDatalogs(){
+    axios.get("/datalogs")
+      .then(res=>{
+       // temperature & humidity sensor
+       el('th').innerHTML = "";
+      
+        var count1 = 1;
+       res.data.dht22.forEach(dht=>{
+         el('th').innerHTML += `
+          <tr>
+             <td>${count1}</td>
+             <td>${dht.humidity}</td>
+             <td>${dht.temperature} ℃</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count1++;
+       });
+
+       // ultrasonic sensor
+       var count2 = 1;
+       el('dt').innerHTML = "";
+        res.data.ultrasonic.forEach(dht=>{
+         el('dt').innerHTML += `
+          <tr>
+             <td>${count2}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count2++
+       });
+       // gas sensor
+       var count3 = 1;
+       el('gs').innerHTML = "";
+        res.data.gas.forEach(dht=>{
+         el('gs').innerHTML += `
+          <tr>
+             <td>${count3}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count3++;
+       });
+       // sound sensor
+       var count4 = 1;     
+       el('ss').innerHTML = "";
+       res.data.sound.forEach(dht=>{
+         el('ss').innerHTML += `
+          <tr>
+             <td>${dht.id}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count4++;
+       });
+       // touch sensor
+       var count5 = 1;    
+       el('ts').innerHTML = "";
+        res.data.capacitive.forEach(dht=>{
+         el('ts').innerHTML += `
+          <tr>
+             <td>${count5}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count5++;
+       });
+       // flame sensor
+       var count6 =1;    
+       el('fsx').innerHTML = "";
+        res.data.flame.forEach(dht=>{
+         el('fsx').innerHTML += `
+          <tr>
+             <td>${count6}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count6++;
+       });
+       // raindrop sensor
+      var count7=1;    
+       el('rs').innerHTML = "";
+        res.data.raindrop.forEach(dht=>{
+         el('rs').innerHTML += `
+          <tr>
+             <td>${count7}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count7++;
+       });
+       // magnetic sensor
+       var count8 =0;    
+       el('ms').innerHTML = "";
+        res.data.halleffect.forEach(dht=>{
+         el('ms').innerHTML += `
+          <tr>
+             <td>${count8}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count8++;
+       });
+       // list sensor
+       var count9 = 1;    
+       el('lsx').innerHTML = "";
+        res.data.light.forEach(dht=>{
+         el('lsx').innerHTML += `
+          <tr>
+             <td>${count9}</td>
+             <td>${dht.reading}</td>
+             <td>${dht.date}</td>
+             <td>${dht.time}</td>
+          </tr>
+         `;
+         count9++;
+       });        
+    });
+}
+
 function nav(page,btn){
+   if(page === "datalogs"){
+      parseDatalogs();
+   }
    sclass('active')[0].classList.remove("active");
    btn.classList.add('active');
    el(cpage).style.display = "none";
@@ -37,8 +234,8 @@ function nav(page,btn){
 // Add a random value to each line every second
 setInterval(function() {
   line1.append(new Date().getTime(), parseInt(el('hum').innerHTML));
-  line2.append(new Date().getTime(), temperature);
-  line3.append(new Date().getTime(), sound);
+  line2.append(new Date().getTime(), parseInt(el('temp').innerHTML));
+  line3.append(new Date().getTime(), parseFloat(el('ssx').innerHTML));
 }, 500);
 
 // Add to SmoothieChart
